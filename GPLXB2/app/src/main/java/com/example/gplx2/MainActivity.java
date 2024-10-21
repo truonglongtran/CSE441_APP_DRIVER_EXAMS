5f00e0f10cc07109ac04bbe7d6c7832c3ec2c7a9
truonglongtran <longtran2308bn@gmail.com>
2024-10-21 15:29:39 +0700

    add main activity

--- GPLXB2/app/src/main/java/com/example/gplx2/MainActivity.java
+++ GPLXB2/app/src/main/java/com/example/gplx2/MainActivity.java
@@ -1,12 +1,12 @@
-package com.example.gplx;
+package com.example.gplxb2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +15,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageButton btnRandom = findViewById(R.id.btnrandom);
+        btnRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TestActivity.class);
+                intent.putExtra("examsIndex", -1); // Truyền giá trị -1 cho btnRandom
+                startActivity(intent);
+            }
+        });
+
+        // Thiết lập sự kiện cho nút chính xác
+        ImageButton btnExact = findViewById(R.id.btnexact);
+        btnExact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ExamsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //btn liet
+        ImageButton btnLiet = findViewById(R.id.btnliet);
+        btnLiet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TestActivity.class);
+                intent.putExtra("examsIndex", 60); // Truyền giá trị -1 cho btnRandom
+                startActivity(intent);
+            }
+        });
+
+        //btn btntopfalseds
+        ImageButton btntopfalseds = findViewById(R.id.btntopfalseds);
+        btntopfalseds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TestActivity.class);
+                intent.putExtra("examsIndex", 50); // Truyền giá trị -1 cho btnRandom
+                startActivity(intent);
+            }
+        });
+
+        //btn btntopfalseds
+        ImageButton btnfalseds = findViewById(R.id.btnfalseds);
+        btnfalseds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TestActivity.class);
+                intent.putExtra("examsIndex", 40); // Truyền giá trị -1 cho btnRandom
+                startActivity(intent);
+            }
+        });
+
+        //btn btntrain
+        ImageButton btntrain = findViewById(R.id.btntrain);
+        btntrain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DriverTypeActivity.class);
+                intent.putExtra("examsIndex", 40); // Truyền giá trị -1 cho btnRandom
+                startActivity(intent);
+            }
         });
     }
 }
